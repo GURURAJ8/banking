6b1f0f85315cd020d4d809ee7571d29998c74401
Gururaj <gururajsaraph11@gmail.com>
2026-01-27 13:01:18 +0530

    Marshaling data structure to JSON representation

--- main.go
+++ main.go
@@ -1,15 +1,31 @@
 package main
 
 import (
-	"fmt"
+	// "fmt"
 	"net/http"
+	"encoding/json"
+	"log"
 )
+type Customer struct{
+	Name string `json:"name"`
+	Zip string `json:"zip"`
+	City string `json:"city"`
+}
+
+//handler 
 
 func main(){
-	http.HandleFunc("/", greet)
+	// http.HandleFunc("/", greet)
+	http.HandleFunc("/customers", getAllCustomers)
 	log.Fatal(http.ListenAndServe("localhost:8080", nil))
 }
 
-func greet(w http.ResponseWriter, r *http.Request){
-		fmt.Fprintf(w, "Hello, World!")
+func getAllCustomers(w http.ResponseWriter, r *http.Request){
+		customer:= []Customer{
+			{Name: "John Doe", Zip: "12345", City: "New York"},
+			{Name: "Jane Smith", Zip: "67890", City: "Los Angeles"},
+			{Name: "Alice Johnson", Zip: "54321", City: "Chicago"},	
+		}
+	json.NewEncoder(w).Encode(customer)
+	w.Header().Set("Content-Type", "application/json")
 	}
