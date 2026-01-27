bacae279ab27cbad6a4dc4c50c7f036e3f517c72
Gururaj <gururajsaraph11@gmail.com>
2026-01-27 13:10:39 +0530

    Marshaling data structure into XML representation

--- main.go
+++ main.go
@@ -6,13 +6,6 @@ import (
 	"encoding/json"
 	"log"
 )
-type Customer struct{
-	Name string `json:"name"`
-	Zip string `json:"zip"`
-	City string `json:"city"`
-}
-
-//handler 
 
 func main(){
 	// http.HandleFunc("/", greet)
@@ -20,12 +13,30 @@ func main(){
 	log.Fatal(http.ListenAndServe("localhost:8080", nil))
 }
 
+//Data Transfer Object
+type Customer struct{
+	Name string `json:"name" xml:"name"`
+	Zip string `json:"zip" xml:"zip"`
+	City string `json:"city" xml:"city"`
+}
+
+//handler functions
 func getAllCustomers(w http.ResponseWriter, r *http.Request){
 		customer:= []Customer{
 			{Name: "John Doe", Zip: "12345", City: "New York"},
 			{Name: "Jane Smith", Zip: "67890", City: "Los Angeles"},
 			{Name: "Alice Johnson", Zip: "54321", City: "Chicago"},	
 		}
-	json.NewEncoder(w).Encode(customer)
-	w.Header().Set("Content-Type", "application/json")
-	}
+
+	if r.Header.Get("Content-Type") == "application/xml" {
+		//Marshal to XML and write to response
+		xml.NewEncoder(w).Encode(customer)
+		w.Header().Set("Content-Type", "application/xml")
+		return
+	}else{
+		// Default to JSON if no Content-Type is specified
+		json.NewEncoder(w).Encode(customer)
+		w.Header().Set("Content-Type", "application/json")
+		return
+	}	
+}
