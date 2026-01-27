eccd8330ee78a369fe03ce7dde3b71416b2fb786
Gururaj <gururajsaraph11@gmail.com>
2026-01-27 13:47:47 +0530

    gorilla/mux package installed and added to app.go

--- app/app.go
+++ app/app.go
@@ -4,10 +4,13 @@ import (
 	// "fmt"
 	"net/http"
 	"log"
+	"github.com/gorilla/mux"
 )
 
 func Start(){
 	// http.HandleFunc("/", greet)
-	http.HandleFunc("/customers", getAllCustomers)
-	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+	// http.HandleFunc("/customers", getAllCustomers)
+	router:=mux.NewRouter()
+	router.HandleFunc("/customers", getAllCustomers).Methods("GET")
+	log.Fatal(http.ListenAndServe("localhost:8080", router))
 }
